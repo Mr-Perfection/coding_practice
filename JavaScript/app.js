2eba4b59e38d67b419c38fce6f4df39a5f35ad36
Stephen Lee <stephenslee0127@gmail.com>
2017-01-29 18:47:09 -0800

    merge conflit

--- JavaScript/app.js
+++
@@ -1,8 +0,0 @@
-/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */ 
-/*global define */
-
-var a = 'stephen lee';
-
-function b() {   
-    console.log("hel");
-}
